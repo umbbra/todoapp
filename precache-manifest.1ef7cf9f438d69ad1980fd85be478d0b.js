d45fd007209ca201a89dc58c56fe421615eb64f8
Patrycja Paczkowska <umbra@spoko.pl>
2019-08-07 12:09:32 +0200

    Updates

---
+++ precache-manifest.1ef7cf9f438d69ad1980fd85be478d0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1514be25cfef50f6d86d6a5a9e63be",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "e734a851943ca723a457",
+    "url": "/todoapp/static/css/main.6abcac0e.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/todoapp/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "e734a851943ca723a457",
+    "url": "/todoapp/static/js/main.15fdcf51.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
